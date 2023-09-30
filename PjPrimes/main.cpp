11167d31cc4be4fbd04294078ee342c57ea8f380
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-09-29 22:51:54 -0700

    Initial Commit

---
+++ PjPrimes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PjPrimes
+//
+//  Created by Jacob Yeung on 9/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
